9d5b4e7e110b83122b0343847b2c1f385fb0fe96
Arthur Zheng <arthurzme@gmail.com>
2023-12-21 11:18:31 -0800

    Fixed image ratio on large screens then, I hid it...

--- app/components/bookSummaryList.tsx
+++ app/components/bookSummaryList.tsx
@@ -26,29 +26,37 @@ const BookSummaryList: React.FC<BookListProps> = ({ bookSummaries }) => {
       {bookSummaries.map((bookSummary, index) => (
         <div
           key={index}
-          className="flex border-t-2 border-black last:border-b-2 pr-6"
+          className="flex items-stretch border-t-2 border-black last:border-b-2 pr-6"
         >
-          <Image
-            width={430}
-            height={337}
+          {/* DO NOT DELETE */}
+          {/* <Image
+            width={213}
+            height={276}
             src={bookSummary.cover}
-            className="border-black border-l-3 border-r-2"
-            alt={`Book cover for ${bookSummary.title}`}
-          />
+            className="border-black border-l-3 border-r-2 object-cover aspect-[3/4] shrink-0 grow-0 max-auto max-h-full"
+            alt={`Book cover image for ${bookSummary.title}`}
+          /> */}
+          {/* DO NOT DELETE */}
           <div className="pl-6 py-4 flex-auto">
-            <div className="flex px-2">
-              <div className="flex-auto text-xl">
-                <span className="font-semibold">{bookSummary.title}</span>
-                <span className="pl-2">{`(${bookSummary.author})`}</span>
+            <div className="flex px-2 items-center">
+              <div className="flex-auto text-xl py-2">
+                <span className="text-2xl font-semibold">
+                  {bookSummary.title}
+                </span>
+                <span className="pl-2">{` (${bookSummary.author})`}</span>
               </div>
               {bookSummary.tags.map((tag, index) => (
-                <span className="font-bold inline-block pl-2" key={index}>
+                <span className="font-bold inline-block pl-2 py-2" key={index}>
                   {tag}
                 </span>
               ))}
             </div>
-            <p className="p-2 text-lg text-justify">{bookSummary.summary}</p>
-            <Link href={`/book/${bookSummary.id}`}>继续阅读</Link>
+            <p className="p-2 text-lg text-justify">
+              {bookSummary.summary}
+              <Link className="underline" href={`/book/${bookSummary.id}`}>
+                詳情
+              </Link>
+            </p>
           </div>
         </div>
       ))}
