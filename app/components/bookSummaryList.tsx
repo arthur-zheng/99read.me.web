14fe69212a1c028486e8a2a68c23621bfc544fcb
Arthur Zheng <arthurzme@gmail.com>
2023-12-20 22:59:19 -0800

    Styles for summary list

--- app/components/bookSummaryList.tsx
+++ app/components/bookSummaryList.tsx
@@ -22,22 +22,30 @@ const BookSummaryList: React.FC<BookListProps> = ({ bookSummaries }) => {
   return (
     <div>
       {bookSummaries.map((bookSummary, index) => (
-        <div key={index} className="flex">
+        <div
+          key={index}
+          className="flex border-t-2 border-black last:border-b-2"
+        >
           <Image
-            width={400}
+            width={430}
             height={337}
             src={bookSummary.cover}
+            className="border-black border-l-3 border-r-2"
             alt={`Book cover for ${bookSummary.title}`}
           />
-          <div className="pl-6 py-4">
+          <div className="pl-6 py-4 flex-auto">
             <div className="flex px-2">
-              <div className="flex-auto text-lg">
+              <div className="flex-auto text-xl">
                 <span className="font-semibold">{bookSummary.title}</span>
-                <span className="">{`(${bookSummary.author})`}</span>
+                <span className="pl-2">{`(${bookSummary.author})`}</span>
               </div>
-              {bookSummary.tags}
+              {bookSummary.tags.map((tag, index) => (
+                <span className="font-bold inline-block pl-2" key={index}>
+                  {tag}
+                </span>
+              ))}
             </div>
-            <p className="p-2">{bookSummary.summary}</p>
+            <p className="p-2 text-lg">{bookSummary.summary}</p>
           </div>
         </div>
       ))}
