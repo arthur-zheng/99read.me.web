943738ad13d08f42baf3b19bb14b546ca823625b
Arthur Zheng <arthurzme@gmail.com>
2023-12-24 18:48:44 -0800

    Add TODO in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Header from "./components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// TODO: add book name inside title like: 久久閱讀-千秋夢
+// TODO: add page number inside description
 export const metadata: Metadata = {
   title: "久久讀書",
   description: "最全小說免費讀",
