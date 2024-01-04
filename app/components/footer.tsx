9359884ea5fa68dd1ffd28d07f82a637d4110de8
Arthur Zheng <arthurzme@gmail.com>
2024-01-04 10:04:47 -0800

    footer text style fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { legal } from "../libs/constants";
 
 const Footer = () => {
-  return <footer className="pl-8 py-10 text-center">{legal}</footer>;
+  return <footer className="pl-2 py-8 text-center lg:px-8">{legal}</footer>;
 };
 
 export default Footer;
