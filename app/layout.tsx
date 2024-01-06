d23b01271e2f6c39666e686dcafb38b25a6a0a76
Arthur Zheng <arthurzme@gmail.com>
2024-01-05 23:19:14 -0800

    disallow bots

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,19 +24,19 @@ export const metadata: Metadata = {
     "天官赐福小说",
     "纯爱小说",
   ],
-  // robots: {
-  //   index: true,
-  //   follow: false,
-  //   nocache: true,
-  //   googleBot: {
-  //     index: false,
-  //     follow: false,
-  //     noimageindex: true,
-  //     "max-video-preview": -1,
-  //     "max-image-preview": "large",
-  //     "max-snippet": -1,
-  //   },
-  // },
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
