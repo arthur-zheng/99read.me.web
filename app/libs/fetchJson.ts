69b1e9c5d9b3f54d0498d5bb32349ef19840c90c
Arthur Zheng <arthurzme@gmail.com>
2024-01-22 22:42:10 -0800

    using env

--- app/libs/fetchJson.ts
+++ app/libs/fetchJson.ts
@@ -1,12 +1,7 @@
-const PROD_ENDPOINT = "https://www.99read.me:5001";
-const DEV_ENDPOINT = "http://127.0.0.1:5001";
-
 // fetch based on url
 export async function fetchJson<T>(url: string): Promise<T> {
   // export async function fetch(url: string, env = "prod"): any {
-  url =
-    (process.env.NODE_ENV === "development" ? DEV_ENDPOINT : PROD_ENDPOINT) +
-    url;
+  url = process.env.ENDPOINT + url;
   const res = await fetch(url);
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
